641d196b03a54bf184af5bc90709eac98f08d564
Rostyslav Diachok <infernalmaster1@gmail.com>
2017-12-28 00:25:06 +0200

    remake delete button as form

--- src/helpers/form_helpers.cr
+++ src/helpers/form_helpers.cr
@@ -17,4 +17,36 @@ module FormHelpers
   def uploaded_path(name : String | Nil)
     "/uploads/#{name}"
   end
+
+  def delete_button_to(body : String, url : String, **options : Object)
+    button_to(body, url, :delete, **options) do
+      csrf_tag
+    end
+  end
+
+  # BEGIN delete this after new release of jasper_helpers
+  def button_to(body : String, url : String, method : Symbol = :post)
+    form(action: url, method: method) do
+      content(:button, body, {:type => "submit"})
+    end
+  end
+
+  def button_to(body : String, url : String, method : Symbol = :post)
+    form(action: url, method: method) do
+      String.build do |str|
+        str << yield
+        str << content(:button, body, {:type => "submit"})
+      end
+    end
+  end
+
+  def button_to(body : String, url : String, method : Symbol = :post, **options : Object)
+    form(action: url, method: method) do
+      String.build do |str|
+        str << yield
+        str << content(:button, body, options.to_h)
+      end
+    end
+  end
+  # END delete this after new release of jasper_helpers
 end
