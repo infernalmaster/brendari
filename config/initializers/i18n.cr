626f91cbe6c7371c3bafb9795a9fc0d8d6c5736f
Rostyslav Diachok <infernalmaster1@gmail.com>
2018-03-13 23:35:07 +0200

    update amber

--- config/initializers/i18n.cr
+++ config/initializers/i18n.cr
@@ -1,6 +1,24 @@
-require "i18n"
+require "citrine-i18n"
 
-I18n.load_path += ["config/locales"]
+Citrine::I18n.configure do |settings|
+  # Backend storage (as supported by i18n.cr)
+  # settings.backend = I18n::Backend::Yaml.new
+
+  # Default locale (defaults to "en" and "./src/locales/**/en.yml").
+  # For a new default locale to be accepted, it must be found by the
+  # backend storage and reported in "settings.available_locales".
+  settings.default_locale = "en"
+
+  # Separator between sublevels of data (defaults to '.')
+  # e.g. I18n.translate("some/thing") instead of "some.thing"
+  # settings.default_separator = '.'
+
+  # Returns the current exception handler. Defaults to an instance of
+  # I18n::ExceptionHandler.
+  # settings.exception_handler = ExceptionHandler.new
+
+  # The path from where the translations should be loaded
+  settings.load_path += ["config/locales"]
+end
+
 I18n.init
-
-I18n.default_locale = "en" # default can be set after loading translations
--- config/routes.cr
+++ config/routes.cr
@@ -2,13 +2,16 @@ Amber::Server.configure do |app|
   pipeline :web, :admin do
     # Plug is the method to use connect a pipe (middleware)
     # A plug accepts an instance of HTTP::Handler
+    plug Amber::Pipe::PoweredByAmber.new
+    # plug Amber::Pipe::ClientIp.new(["X-Forwarded-For"])
+    plug Citrine::I18n::Handler.new
     plug Amber::Pipe::Error.new
     plug Amber::Pipe::Logger.new
     plug Amber::Pipe::Session.new
     plug Amber::Pipe::Flash.new
     plug Amber::Pipe::CSRF.new
     plug HTTP::CompressHandler.new
-    plug LocaleSetter.new
+    plug Amber::Pipe::Reload.new if Amber.env.development?
   end
 
   pipeline :admin do
@@ -17,6 +20,8 @@ Amber::Server.configure do |app|
 
   # All static content will run these transformations
   pipeline :static do
+    plug Amber::Pipe::PoweredByAmber.new
+    # plug Amber::Pipe::ClientIp.new(["X-Forwarded-For"])
     plug Amber::Pipe::Error.new
     plug CacheHeader.new
     plug Amber::Pipe::Static.new "./public"
